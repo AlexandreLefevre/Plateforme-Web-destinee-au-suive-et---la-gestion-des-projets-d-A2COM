2d1b27166bca6495c0a3924b23734f25defc1cb2
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2021-04-02 12:24:54 +0200

    Premier commit

---
+++ Archives.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Archives</title>
+    <link rel="stylesheet" href="styles/bootstrap-337.min.css">
+    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" href="images/Favicon-A2com.ico">
+    
+</head>
+<body>
+    
+
+    
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+    
+    <div id="navbar" class="navbar navbar-default">
+        <div class="container"> <!-- container begin -->
+          
+           
+           <div class="navbar-header">   <!-- navbar begin -->
+               
+               
+               <a href="index.php" class="navbar-brand home">   <!-- navbar-brand home begin -->
+                 
+                  <img src="images/logo_A2Com.png" alt="A2com Logo" class="hidden-xs">
+                  <img src="images/logo_A2Com.png" alt="A2com Logo mobile" class="visible-xs">
+                   
+                   
+               </a><!-- navbar-brand home finish -->
+                 
+                 <button class="navbar-toggle" data-toggle="collapse" data-target="#navigation">
+                     
+                     <span class="sr-only">Toggle Navigation</span>
+                     
+                     <i class="fa fa-align-justify"></i>
+                     
+                 </button>
+                 
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#search">
+                     
+                     <span class="sr-only">Toggle Search</span>
+                     
+                     <i class="fa fa-search"></i>
+                     
+                 </button>
+               
+               
+           </div> <!-- navbar header finish -->
+             
+             <div class="navbar-collapse collapse" id="navigation"> <!-- navbar-collapse collapse begin -->
+             
+                <div class="paggind-nav"><!-- padding-nav begin -->
+                   
+                   <ul class="nav navbar-nav left"><!-- nav navbar-nav left begin -->
+                       
+                       <li>
+                           <a href="index.php">Dashboard</a>
+                       </li>
+                       <li>
+                           <a href="ProjetEnCous.php">Projet en cours</a>
+                       </li>
+                       <li>
+                           <a href="MiniProjet.php">Mini-Projet</a>
+                       </li>
+                       <li>
+                           <a href="ProjetVideo.php">Projet video</a>
+                       </li>
+                       <li class="active">
+                           <a href="Archives.php">Archives</a>
+                       </li>
+                       
+                   </ul><!-- nav navbar-nav left finish -->
+                    
+                </div><!-- padding-nav finisg -->
+                   
+                 
+             </div><!-- navbar-collapse collapse finish -->
+              
+            
+        </div><!-- container finish -->
+        
+    
+    </div><!--navbar navbar-default Finish -->
+    
+</body>
+</html>

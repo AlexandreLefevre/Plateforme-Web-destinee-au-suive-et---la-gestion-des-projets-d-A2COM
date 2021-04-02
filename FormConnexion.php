0a90ef3a576ee3f29642d0c2dd713675645ff3b6
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2021-04-02 15:41:17 +0200

    suite

--- FormConnexion.php
+++ FormConnexion.php
@@ -8,7 +8,32 @@
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
             <title>Connexion</title>
         </head>
-        <body> 
+        <body>
+        <?php 
+                if(isset($_GET['login_err']))
+                {
+                    $err = htmlspecialchars($_GET['login_err']);
+
+                    switch($err)
+                    {
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email incorrect
+                            </div>
+                        <?php
+                        break;
+
+                        case 'already':
+                        ?>   
         <div class="login-form">        
             <form action="connexion.php" method="post">
                 <h2 class="text-center">Connexion</h2>       

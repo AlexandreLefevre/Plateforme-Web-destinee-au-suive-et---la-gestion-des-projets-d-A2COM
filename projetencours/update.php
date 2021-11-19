17c0c5a7ac963942a46c86ead49bfb63451b7c79
AlexandreLefevre <44492979+AlexandreLefevre@users.noreply.github.com>
2021-11-19 13:25:44 +0100

    Update update.php

--- projetencours/update.php
+++ projetencours/update.php
@@ -1,11 +1,13 @@
 <?php
 require_once '../config.php';
 
+if(isset($_POST['column_name']) && isset($_POST['value']) && isset($_POST['id']){
 $column_name = mysqli_real_escape_string($db, $_POST['column_name']);
 $column_value = mysqli_real_escape_string($db, $_POST['value']);
 $record_id = intval($_POST['id']);
+}
 
-if(isset($record_id))
+if(isset($_POST["id"]))
 {
 
  if( $column_name == 'facturation1' or $column_name == 'facturation2' or $column_name == 'facturation3'
